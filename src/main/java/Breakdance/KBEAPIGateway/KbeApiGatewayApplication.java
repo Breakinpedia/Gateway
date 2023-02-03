9ffaddc46d26458289439edc3e75b45cebf81e0b
Thien Duong <duongthien1809@gmail.com>
2023-02-03 22:13:05 +0100

    first commit

---
+++ src/main/java/Breakdance/KBEAPIGateway/KbeApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Breakdance/KBEAPIGateway/KbeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
